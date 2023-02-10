85d14b0c30c70a517c7a8fe293f9690e3b3f0cc7
danielocampo226 <sierradanny562@gmail.com>
2023-02-10 14:56:47 -0500

    set6

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -7,37 +7,128 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            return ((startNumber / n) + 1) * n;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < businesses.Length; i++)
+            {
+                if (businesses[i].TotalRevenue == 0)
+                {
+                    businesses[i].Name = "CLOSED";
+                }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            var sum = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+
+            string sentence = "";
+
+            foreach (string word in words)
+            {
+                if (word.Trim().Length > 0)
+                {
+                    sentence += word.Trim() + " ";
+                }
+                //trim method is not permanent in the if statement,
+                //so we write it again. the + " "; adds a space
+                //to the word
+            }
+
+            if (sentence.Length == 0)
+            {
+                return "";
+            }
+
+            //we add trim(); again because it will have an extra 
+            //space at the end we don't need because of trim above
+            sentence = sentence.Trim();
+            sentence += ".";
+            return sentence;
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            if (elements == null || elements.Count == 0)
+            {
+                return new double[0];
+                //adding new to double makes the 0
+                //in the brackets mean 0 elements vs
+                //the first index of the array
+            }
+
+            List<double> everyFourth = new List<double>();
+
+            for (var i = 3; i < elements.Count; i += 4)
+            {
+                everyFourth.Add(elements[i]);
+            }
+
+            return everyFourth.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                //int j is then index 1 and i is index 0,
+                //int j is not adding index 0 and index 1
+                //together. nested forloops allows us to check
+                //one element against another element
+                {
+                    if (nums[i] + nums[j] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
-}
+}    
+
